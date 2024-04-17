07db36ad7ec89fb9dcc26952cc0bc0635851a1b2
giugutto <g.guttoriello96@gmail.com>
2024-04-17 19:11:24 +0200

    Aggiornamento con lezione 2.6

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -24,3 +24,30 @@ cap: str = name.capitalize()
 superstringa = (f"Il nome in lettere minuscole è {low}, quello a lettere maiuscole è {up} e infine con la prima lettera maiuscola è {cap}")
 
 print(superstringa)
+
+#2-5. Famous Quote: Find a quote from a famous person you admire.
+#Print the quote and the name of its author. Your output should look something like the following, 
+#including the quotation marks: Albert Einstein once said, 
+#“A person who never made a mistake never tried anything new.”
+
+name: str = "Elliot Alderson"
+quote: str = "\"Hackers...they never trust each other\""
+
+stringafinale: str = (f"{name} in one of the episode said {quote}")
+
+print(stringafinale)
+
+
+# 2-6. Famous Quote 2: Repeat Exercise 2-5,
+# but this time, represent the famous person’s name using a variable called famous_person. 
+# Then compose your message and represent it with a new variable called message.
+#  Print your message.
+
+
+
+famous_person = name
+
+message: str = (f"{famous_person} in one of the episode said {quote}")
+
+print(message)
+
