97b61c835b9b6db82bb8b22dcf6814104f784d82
giugutto <g.guttoriello96@gmail.com>
2024-04-17 13:49:05 +0200

    File lezione2.py aggiunto

---
+++ Lezione2/lezione2.py
@@ -0,0 +1,14 @@
+#Giuseppe Guttoriello
+#17/07/24
+
+#print("Hello World!")
+
+#2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person.
+#Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+name: str = "Gabriele"
+
+message: str = f"Hello {name}, would you like to drink something today?"
+
+print(message)
+
