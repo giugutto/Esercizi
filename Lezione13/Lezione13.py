f94b9973ed906977acf56becd3938f828de65a73
giugutto <g.guttoriello96@gmail.com>
2024-05-30 13:43:23 +0200

    Esercizi aggiornato

--- Lezione13/Lezione13.py
+++ Lezione13/Lezione13.py
@@ -78,28 +78,36 @@ class Book:
         if self.is_borrowed == True:
             self.is_borrowed = False
 
+    def __str__(self) -> str:
+        return self.title
+
 
 
 
 class Member:
-    def __init__(self, member_id: str,name: str, borrowed_book: list[Book] = []) -> None:
+    def __init__(self, member_id: str,name: str) -> None:
         self.member_id = member_id
         self.name = name
-        self.borrowed_book: list = borrowed_book
+        self.borrowed_book: list = []
 
 
     def borrow_book(self, book: Book):
-        self.borrowed_book.append(book)
+        if book.is_borrowed == False:
+            self.borrowed_book.append(book)
+            book.borrow()
+       
         
             
     def return_book(self, book:Book):
         self.borrowed_book.remove(book)
+        book.return_book()
+
 
 
 class Library:
-    def __init__(self, books: dict[str, Book] = {}, members:dict[str, Member] = {}) -> None:
-        self.books = books
-        self.members = members
+    def __init__(self) -> None:
+        self.books = {}
+        self.members = {}
 
 
     def add_book(self, book_id: str, title: str, author: str):
@@ -109,24 +117,154 @@ class Library:
         member = Member(member_id, name)
         self.members[member_id] = member
     def borrow_book(self, member_id: str, book_id: str):
-        if book_id in self.books and not self.books[book_id].is_borrowed:
-            self.books[book_id].borrow()
-            self.members[member_id].borrow_book(self.books[book_id])
+        if book_id in self.books:
+            if not self.books[book_id].is_borrowed:
+                if member_id in self.members:
+                    self.members[member_id].borrow_book(self.books[book_id])
+                else: 
+                    raise ValueError('Member not found')
+            else:
+                raise ValueError("Book is already borrowed")
         else:
-            raise ValueError()
+            raise ValueError('Book not found')
     def return_book(self, member_id: str, book_id: str):
         if member_id in self.members and book_id in self.books:
             member = self.members[member_id]
             book = self.books[book_id]
-            member.return_book(book)
-        else:
-            raise ValueError()
+            if book.is_borrowed:
+                member.return_book(book)
+            else:
+                raise ValueError("Book not borrowed by this member")
         
     def get_borrowed_books(self, member_id) -> list[Book]: # restituisce la lista dei libri presi in prestito dal membro.
-        print(self.members[member_id].borrowed_book)
+        return [i.__str__() for i in self.members[member_id].borrowed_book]
+
+
+library = Library()
+
+library.add_book("B001", "The Great Gatsby", "F. Scott Fitzgerald")
+library.add_book("B002", "1984", "George Orwell")
+library.add_book("B003", "To Kill a Mockingbird", "Harper Lee")
+
+# Register members
+library.register_member("M001", "Alice")
+library.register_member("M002", "Bob")
+library.register_member("M003", "Charlie")
+
+# Borrow books
+library.borrow_book("M001", "B001")
+library.borrow_book("M002", "B002")
+
+
+ # Return books
+library.return_book("M001", "B001")
+library.return_book("M002", "B002")
+
+
+# Edge case - trying to borrow a book by a non-existent member
+try:
+    library.borrow_book("M004", "B001")
+except ValueError as e:
+    print(e)
+
+ 	
+ESERCIZIO 3
+# Data una stringa s e una lista di stringhe wordDict, 
+# restituisce True se s può essere segmentato in una sequenza separata da spazi di una o più parole del dizionario;
+# False altrimenti.
+# Tieni presente che la stessa parola nel dizionario può essere riutilizzata più volte nella segmentazione.
+# print(word_break("leetcode",["leet","code"]))
+# True
+# print(word_break("applepenapple", ["apple","pen"]))
+# True
+# print(word_break("catsandog",["cats","dog","sand","and","cat"]))
+# False
+def world_break(stringa: str, lista: list):
     
 
+ESERCIZIO 4
+# Data l'inizio di una lista concatenata, invertire la lista e restituire la lista invertita.
 
+# For example:
+# Test 	Result
 
+# head = ListNode(val=1, next=ListNode(val=2, next=ListNode(val=3, next=ListNode(val=4, next=ListNode(val=5)))))
+# print(reverse_list(head))
 
+	
 
+# [5, 4, 3, 2, 1]
+
+# head = ListNode(val=1, next=ListNode(val=2))
+# print(reverse_list(head))
+
+ESERCIZIO 6
+# Determina se una tavola Sudoku 9 x 9 è valida. Solo le celle compilate devono essere convalidate secondo le seguenti regole:
+
+#     Ogni riga deve contenere le cifre 1-9 senza ripetizioni.
+#     Ciascuna colonna deve contenere le cifre da 1 a 9 senza ripetizioni.
+#     Ciascuno dei nove sottoriquadri 3 x 3 della griglia deve contenere le cifre 1-9 senza ripetizione.
+
+# Nota:
+
+#     Una tavola Sudoku (parzialmente riempita) potrebbe essere valida ma non è necessariamente risolvibile.
+#     Solo le celle riempite devono essere convalidate secondo le regole menzionate.
+ESERCIZIO 7
+# Date due stringhe s e t, restituire True se t è un anagramma di s, e False altrimenti.
+
+# Un anagramma è una parola o una frase formata riorganizzando le lettere di una parola o frase diversa, in genere utilizzando tutte le lettere originali esattamente una volta.
+
+# For example:
+# Test 	Result
+
+# print(anagram("anagram","nagaram"))
+
+	
+
+# True
+ESERCIZIO BANCA
+# Progettare un semplice sistema bancario con i seguenti requisiti:
+
+#     Classe del Account:
+#         Attributi:
+#             account_id: str - identificatore univoco per l'account.
+#             balance: float - il saldo attuale del conto.
+#         Metodi:
+#             deposit(amount: float) - aggiunge l'importo specificato al saldo del conto.
+#             get_balance(): restituisce il saldo corrente del conto.
+#     Classe Bank:
+#         Attributi:
+#             accounts: dict[str, Account] - un dizionario per memorizzare gli account in base ai loro ID.
+#         Metodi:
+#             create_account(account_id): crea un nuovo account con l'ID specificato e un saldo pari a 0.
+#             deposit(account_id, amount): deposita l'importo specificato sul conto con l'ID fornito.
+#             get_balance(account_id): restituisce il saldo del conto con l'ID specificato.
+
+# For example:
+# Test 	Result
+
+# bank = Bank()
+# account1 = bank.create_account("123")
+# print(account1.get_balance())
+
+	
+
+# 0
+
+# bank = Bank()
+# account1 = bank.create_account("123")
+# bank.deposit("123",100)
+# print(bank.get_balance("123"))
+
+	
+
+# 100
+
+# bank = Bank()
+# account2 = bank.create_account("456")
+# bank.deposit("456",200)
+# print(bank.get_balance("456"))
+
+	
+
+# 200
--- Lezione8/Lezione8.py
+++ Lezione8/Lezione8.py
@@ -1,3 +1,43 @@
 #Lezione 8
 #EREDITARIETÀ E POLISMOSFIRMO
 
+class Operazione:
+    def __init__(self, operando) -> None:
+        self.operando = operando
+
+class OperazioneBinaria(Operazione):
+    def __init__(self, operando, arg1, arg2) -> None:
+        super().__init__(operando)
+        self.arg1 = arg1
+        self.arg2 = arg2
+    #stiamo aggiungendo attributi alla sottoclasse operazionebinaria che prende l'attributo operando della superclasse
+    def set_arg1(self,arg1): #setter
+        self.arg1 = arg1
+
+    def set_arg2(self, arg2): #setter
+        self.arg2 = arg2
+
+    def get_arg1(self): #getter
+        return self.arg1
+    
+    def get_arg2(self): #getter
+        return self.arg2
+        
+    def somma(self):
+        return self.arg1 + self.arg2
+    def esegui_operazione(self):
+        if self.operando == '+':
+            return self.somma()
+        
+    #il metodo statico prende degli input e eresituisce un output ma non accede agli attributi della classe
+    #il metodo statico non accede allo stato dell'oggetto non avendo il self
+    @staticmethod
+    def static_sum(arg1: int, arg2:int):
+        return arg1 + arg2
+    def esegui_operazione(self):
+        if self.operando == '+':
+            return OperazioneBinaria.static_sum(self.arg1, self.arg2)
+        
+op = OperazioneBinaria('+', 5, 23)
+print(op.esegui_operazione())
+
