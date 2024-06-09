e9c92fa353b91a3ccd56734f6985028f505d8bf7
giugutto <g.guttoriello96@gmail.com>
2024-06-09 21:13:09 +0200

    Esercizi aggiornato

--- Esercizirandom/Esercizi _casuali.py
+++ Esercizirandom/Esercizi _casuali.py
@@ -15,3 +15,47 @@ for i in tup:
         continue
 
 print(varcon)
+#Scrivi una funzione che prende una lista di parole e restituisce la parola più lunga.
+def lunga(lista)-> list:
+    parola = ""
+    for i in lista:
+        if len(i) > len(parola):
+            parola = i
+        else:
+            continue
+    return parola
+
+print(lunga(["ciao","mamma","prova","gallo","galline","patata"]))
+
+
+#Given a list of numbers, write a program to: Swap the first and last elements of the list and print the modified list.
+numbers = [1, 2, 3, 4, 5]
+secchiello1 = ""
+secchiello2 = ""
+secchiello1 = numbers[0]
+secchiello2 = numbers[4]
+numbers[0] = secchiello2
+numbers[4] = secchiello1
+print(numbers)
+
+
+# Exercise 10: Finding Maximum and Minimum by Index
+# Given a list of integers, write a program to:
+
+# Find and print the index of the maximum value.
+# Find and print the index of the minimum value.
+
+numbers = [34, 12, 56, 78, 23, 89, 3, 45]
+massimo = 1
+minimo = 100
+for i in numbers:
+    if i > massimo:
+        massimo = i
+    else:
+        continue
+for i in numbers:
+    if i < minimo:
+        minimo = i
+    else:
+        continue
+print(f"Il valore massimo è {massimo}, il valore minimo è {minimo}")
