f70f72397bce8ad54344ead04a29d52d50d363f5
giugutto <g.guttoriello96@gmail.com>
2024-06-24 20:31:50 +0200

    Esercizi aggiornato

--- Lezione20/Exceptions.py
+++ Lezione20/Exceptions.py
@@ -167,3 +167,5 @@ print(interactive_calculator("1 ","-"," 3"))
 
 
  #7  Custom Exception for Data Structure Integrity: Define a custom exception class DataStructureIntegrityError.  Define the custom data structure linked list use classes with methods to append, remove and access a given element, and write functions that operate on that (i.e., print the list,  reverse the list, and check whether the list is ordered). Raise this exception if the data structure's integrity is compromised (e.g., empty list access, index error).
+
+
