01cb4186f6f80ea54462eed9eba1a6394ef876df
giugutto <g.guttoriello96@gmail.com>
2024-07-07 21:06:38 +0200

    Esercizi aggiornato

--- Esercizirandom/Esercizi _casuali.py
+++ Esercizirandom/Esercizi _casuali.py
@@ -59,3 +59,16 @@ for i in numbers:
     else:
         continue
 print(f"Il valore massimo è {massimo}, il valore minimo è {minimo}")
+
+
+
+rows = 5
+k = 2 * rows -2
+
+for i in range(rows, -1, -1):
+    for j in range(k, 0, -1):
+        print(end='')
+    #k = k + 1
+    for j in range(0, i + 1):
+        print('*', end='')
+    print("")
